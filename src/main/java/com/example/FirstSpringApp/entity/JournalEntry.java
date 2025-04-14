b49ff9c2f74e9f3deef841803124389ff423b937
Mukesh <mukeshsardiwal2003@gmai.com>
2025-04-14 11:18:27 +0530

    Lombok Added

--- src/main/java/com/example/FirstSpringApp/entity/JournalEntry.java
+++ src/main/java/com/example/FirstSpringApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.example.FirstSpringApp.entity;
 
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,47 +9,13 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 @Document(collection = "journal_entries")
+@Data
+
 public class JournalEntry
 {
     @Id
     private ObjectId id;
     private String title;
     private LocalDateTime date;
-
-
     private String content;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
 }
